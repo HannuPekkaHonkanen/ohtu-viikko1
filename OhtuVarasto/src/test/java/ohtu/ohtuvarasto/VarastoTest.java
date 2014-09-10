833cb3a4ebc6f702a76b387864f35e9dfb9e8252
HannuPekkaHonkanen <hannu.p.honkanen@helsinki.fi>
2014-09-10 22:27:55 +0300

    testi lisaysLaitetaanNegatiivinenMaara rikottu

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -97,8 +97,8 @@ public class VarastoTest {
         // koska negatiivista määrää ei voi lisätä,
         // saldoa ei muuteta
         // pitää olla 3, mutta rikottu jenkins-harjoittelussa
-        // korjattu nyt
-        assertEquals(3, varasto.getSaldo(), vertailuTarkkuus);
+        // EI korjattu nyt
+        assertEquals(4, varasto.getSaldo(), vertailuTarkkuus);
     }
 
     @Test
